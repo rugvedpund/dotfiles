67c8cf8f5c9dc7f31cb9a5119376a22d0b8407e6
rugvedpund <rugvedpund@gmail.com>
2024-05-24 14:29:26 -0400

    fixes for tmux v1.8

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,6 +1,6 @@
-curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
-    | grep "browser_download_url.*appimage" \
-    | cut -d : -f 2,3 \
-    | tr -d \" \
-    | wget -qi - \
-    && chmod +x tmux.appimage
+#!/bin/bash
+
+# cp ./tmux.conf ~/.config/tmux/tmux.conf
+# alias tmux="tmux -f "~/.config/tmux/tmux.conf""
+echo "alias tmux="tmux -f ~/.config/tmux/tmux.conf"" >> ~/.bashrc
+source ~/.bashrc
